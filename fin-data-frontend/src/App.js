ef63ab58267cd0b06cb32336e052cdc45328b2f3
Parker Ciaramella <43451736+paciaramella@users.noreply.github.com>
2024-07-11 13:14:08 -0400

    install mui

--- fin-data-frontend/src/App.js
+++ fin-data-frontend/src/App.js
@@ -2,44 +2,51 @@
 
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import TextField from '@mui/material/TextField';
+import { Container, Typography, Box, Button } from '@mui/material'; // Importing additional MUI components if needed
 
-const MarketCapData = () => {
-  const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://127.0.0.1:5000/company/historical-market-capitalization/AAPL');
-        setData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchData();
-  }, []);
+const MainComponent = () => {
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get('http://127.0.0.1:5000/company/historical-market-capitalization/AAPL');
+  //       setData(response.data);
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
-    <div>
-      <h2>Historical Market Capitalization Data</h2>
-      <ul>
-        {data.map((item, index) => (
-          <li key={index}>
-            Date: {item.date}, Market Cap: {item.marketCap}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Container maxWidth="sm">
+    <Box sx={{ mt: 3 }}>
+      <Typography variant="h5" gutterBottom>
+        Financial Data App
+      </Typography>
+      <TextField
+        label="Please type in a company ticker and we will get some info for you."
+        variant="outlined"
+        fullWidth
+        // Additional props as needed
+      />
+      {/* Other components or buttons can be added here */}
+      <Button variant="contained" color="primary" sx={{ mt: 2, float: "right" }}>
+        Submit
+      </Button>
+    </Box>
+  </Container>
   );
 };
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Financial Data App</h1>
-      </header>
       <main>
-        <MarketCapData />
+        <MainComponent />
       </main>
     </div>
   );
