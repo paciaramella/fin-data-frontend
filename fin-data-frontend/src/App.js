4a7885e448cf59bcd3cbd9e2d29deafcbf1bc806
Parker Ciaramella <43451736+paciaramella@users.noreply.github.com>
2024-07-11 14:50:48 -0400

    Basic getCompanyProfile resovler

--- fin-data-frontend/src/App.js
+++ fin-data-frontend/src/App.js
@@ -1,54 +1,50 @@
-// src/components/MarketCapData.js
-
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import TextField from '@mui/material/TextField';
-import { Container, Typography, Box, Button } from '@mui/material'; // Importing additional MUI components if needed
-
-
+import React, { useState, useContext } from "react";
+import TextField from "@mui/material/TextField";
+import { Container, Typography, Box, Button } from "@mui/material"; // Importing additional MUI components if needed
+import { CompanyContext, CompanyContextProvider} from './context/CompanyContext.tsx';
 const MainComponent = () => {
-  // const [data, setData] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.get('http://127.0.0.1:5000/company/historical-market-capitalization/AAPL');
-  //       setData(response.data);
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
-
+  const { state, api } = useContext(CompanyContext);
+  const { companyProfile } = state;
+  const { getCompanyProfile } = api;
+  const [symbolSearch, setSymbolSearch] = useState('');
+  console.log('symbol', symbolSearch)
   return (
     <Container maxWidth="sm">
-    <Box sx={{ mt: 3 }}>
-      <Typography variant="h5" gutterBottom>
-        Financial Data App
-      </Typography>
-      <TextField
-        label="Please type in a company ticker and we will get some info for you."
-        variant="outlined"
-        fullWidth
-        // Additional props as needed
-      />
-      {/* Other components or buttons can be added here */}
-      <Button variant="contained" color="primary" sx={{ mt: 2, float: "right" }}>
-        Submit
-      </Button>
-    </Box>
-  </Container>
+      <Box sx={{ mt: 3 }}>
+        <Typography variant="h5" gutterBottom>
+          Financial Data App
+        </Typography>
+        <TextField
+          label="Please type in a company ticker and we will get some info for you."
+          variant="outlined"
+          fullWidth
+          onChange={((e) => setSymbolSearch(e.target.value))}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ mt: 2, float: "right" }}
+          onClick={() => getCompanyProfile(symbolSearch)}
+        >
+          Submit
+        </Button>
+        {companyProfile && (
+          companyProfile.companyName
+        )}
+      </Box>
+    </Container>
   );
 };
 
 function App() {
   return (
-    <div className="App">
-      <main>
-        <MainComponent />
-      </main>
-    </div>
+    <CompanyContextProvider>
+      <div className="App">
+        <main>
+          <MainComponent />
+        </main>
+      </div>
+    </CompanyContextProvider>
   );
 }
 
---
+++ fin-data-frontend/src/context/CompanyContext.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useState } from 'react';
+import axios from "axios";
+
+// Create a context object
+const defaultCompanyState = {
+    defaultCompanyProfile: {},
+};
+
+export const CompanyContext = createContext({});
+
+// Create a provider component
+export const CompanyContextProvider = ({ children }) => {
+  const url = "http://127.0.0.1:5000";  
+  const { defaultCompanyProfile } = defaultCompanyState;
+  const [companyProfile, setCompanyProfile] = useState(defaultCompanyProfile); // Define your initial state here
+
+  const invokeGetCompanyProfile = async (symbol: string) => {
+    try {
+      const response = await axios.get(`${url}/company/profile/${symbol}`);
+      setCompanyProfile(response.data[0]);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+  }
+  // Provide the context value to children components
+  const state = {
+    companyProfile,
+  }
+  
+  const api = {
+    setCompanyProfile,
+    getCompanyProfile: invokeGetCompanyProfile,
+  }
+  return (
+    <CompanyContext.Provider value={{ state, api } }>
+      {children}
+    </CompanyContext.Provider>
+  );
+};
+
